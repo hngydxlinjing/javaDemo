88ef9587a98db2e2b92700b588aedf384e997dca
hngydxlinjing <1259478959@qq.com>
2021-02-01 18:39:00 +0800

    单例枚举类实现

--- src/designModel/创建型模式/单例模式/枚举类加载/DatasourceEnum.java
+++ src/designModel/创建型模式/单例模式/枚举类加载/DatasourceEnum.java
@@ -1,5 +1,9 @@
 package designModel.创建型模式.单例模式.枚举类加载;
 
+import org.junit.Test;
+
+import java.sql.Connection;
+
 /**
  * @version V1.0
  * @author: zouwh
@@ -10,18 +14,31 @@ package designModel.创建型模式.单例模式.枚举类加载;
  * 注意：本内容仅限于中泰证券（上海）资产管理有限公司内部传阅，禁止外泄以及用于其他的商业项目
  */
 public enum DatasourceEnum {
-    DATASOURCE;
-    private DBConnection connection = null;
-    private DatasourceEnum() {
+    /**
+     *单例数据源
+     */
+   dataSourceEnum;
+
+    private DBConnection connection  = null;
+
+    //枚举模式private的构造方法，不用显示声明
+    DatasourceEnum() {
+        System.out.println("初始化数据源。。。。");
         connection = new DBConnection();
     }
+
     public DBConnection getConnection() {
         return connection;
     }
+
     public class DBConnection {}
+
     public static void main(String[] args) {
-        DBConnection con1 = DatasourceEnum.DATASOURCE.getConnection();
-        DBConnection con2 = DatasourceEnum.DATASOURCE.getConnection();
-        System.out.println(con1 == con2);
+        DBConnection datasourceEnum1 = DatasourceEnum.dataSourceEnum.getConnection();
+        DBConnection datasourceEnum2 = DatasourceEnum.dataSourceEnum.getConnection();
+        System.out.println(datasourceEnum1  ==datasourceEnum2 );
     }
+
+
 }
+
